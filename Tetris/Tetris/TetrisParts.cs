d5d664aabd7eeb07ee1908ef2a1790b229b86ba0
kinira <florentina2@abv.bg>
2013-09-02 18:10:02 +0300

    add something

--- Tetris/Tetris/TetrisParts.cs
+++ Tetris/Tetris/TetrisParts.cs
@@ -1,6 +1,20 @@
 ﻿using System;
 
-
+namespace TetrisGame
+{
     class TetrisParts
     {
+        static void DeclaredObjects()
+        {
+            bool[,] fallingObjects = new bool[2, 3];
+
+            for (int i = 0; i < fallingObjects.GetLength(0); i++)
+            {
+                for (int j = 0; j < fallingObjects.GetLength(1); j++)
+                {
+                    Console.WriteLine(fallingObjects[i,j]);
+                }
+            }
+        }
     }
+}
